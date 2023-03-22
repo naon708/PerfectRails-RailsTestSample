655c755128b9496dc9535f4ed5dd2cd7405722c7
naon708 <naoya_nis@icloud.com>
2023-03-22 22:19:56 +0900

    テストの並列度を確認した

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,9 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  ### テストの並列実行数を指定
+  parallelize(workers: :number_of_processors) # number_of_processorsはCPUの数
+  # parallelize(workers: 1) # 並列度を1に指定
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
